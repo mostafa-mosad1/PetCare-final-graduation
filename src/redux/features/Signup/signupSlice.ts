421c41a6cca3dd9c973c113627e0ae514ae1591f
MohammedRady59 <mrady0772@gmail.com>
2024-11-02 14:47:09 +0200

    Initialization redux axios

---
+++ src/redux/features/Signup/signupSlice.ts
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const signuppSlice = createSlice({
+  name: "signup",
+  initialState,
+  reducers: {},
+});
+export default signuppSlice.reducer;
